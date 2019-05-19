98ca78e5deb49bbfc95b15c034f66f4191a8189e
Touka-chan <toukachan@mailbox.org>
2019-05-19 18:31:30 +0200

    Added more APKs
    Change-Id: Ia7233c3cb49cf7b0da7220c9c50896363664eb95

---
+++ vendor_microg.mk
@@ -0,0 +1,16 @@
+PRODUCT_PACKAGES += \
+AdAway \
+additional_repos \
+AFWall \
+Aurora \
+FakeStore \
+FDroid \
+FDroidPrivilegedExtension \
+GCam \
+GmsCore \
+GsfProxy \
+Lawnchair \
+MozillaNlpBackend \
+NominatimNlpBackend \
+OpenWeatherMapWeatherProvider \
+SimpleCalendar \
